b9f7ba836c8ff2eb8568392e12120a82ed6ee7eb
yang <yhunshining@gmail.com>
2021-04-22 23:06:32 +0800

    fix: 支持weui

--- packages/framework-plugin-low-code/src/builder/mp/mp_config.ts
+++ packages/framework-plugin-low-code/src/builder/mp/mp_config.ts
@@ -1,10 +1,10 @@
-import * as path from 'path'
-import { merge, get } from 'lodash'
-import { defaultProjConfig } from '../config/mp'
-import { IWeAppData, IWeAppPage, loopDealWithFn } from '../../weapps-core'
-import { IBuildContext } from './BuildContext'
-import { MP_CONFIG_MODULE_NAME } from '../config'
-import { downloadFile } from '../util/net'
+import * as path from 'path';
+import { merge, get } from 'lodash';
+import { defaultProjConfig } from '../config/mp';
+import { IWeAppData, IWeAppPage, loopDealWithFn } from '../../weapps-core';
+import { IBuildContext } from './BuildContext';
+import { MP_CONFIG_MODULE_NAME } from '../config';
+import { downloadFile } from '../util/net';
 
 /**
  * generate app.json & page.json for mp
@@ -13,126 +13,130 @@ import { downloadFile } from '../util/net'
  * @param appConfigs app config from prop edit panel
  */
 export function generateMpConfig(weapps: IWeAppData[], ctx: IBuildContext) {
-  const appConfig = {}
+  const appConfig = { useExtendedLib: { weui: true } };
   const projConfig: any = merge({}, defaultProjConfig, {
     projectname: 'WeApps-' + ctx.appId,
-  })
+  });
   const pageConfigs = weapps.map((app) => {
-    const pageConfig = {}
+    const pageConfig = {};
     // #1 Get page config from mp config
-    const kbConfig = app.lowCodes.find((m) => m.name === MP_CONFIG_MODULE_NAME)
+    const kbConfig = app.lowCodes.find((m) => m.name === MP_CONFIG_MODULE_NAME);
     if (kbConfig) {
       const { pagesConfigJson = {} } = eval(
         `(${kbConfig.code.replace(/export\s+default/, '')})`
-      )
-      merge(pageConfig, pagesConfigJson)
+      );
+      merge(pageConfig, pagesConfigJson);
     }
 
     // #2 Get page config from page page.data editor UI
-    merge(pageConfig, getAppPagesConfig(app.pageInstanceList))
-    return pageConfig
-  })
+    merge(pageConfig, getAppPagesConfig(app.pageInstanceList));
+    return pageConfig;
+  });
 
   const kbConfig = weapps[0].lowCodes.find(
     (m) => m.name === MP_CONFIG_MODULE_NAME
-  )
+  );
   if (kbConfig) {
     const { projectConfigJson = {}, appJson = {} } = eval(
       `(${kbConfig.code.replace(/export\s+default/, '')})`
-    )
+    );
 
     // # project.config.json, https://developers.weixin.qq.com/miniprogram/dev/devtools/projectconfig.html
-    merge(projConfig, projectConfigJson)
+    merge(projConfig, projectConfigJson);
     const matertialMetaIgnores = ctx.materialLibs
       .filter((lib) => !lib.isComposite)
       .reduce((arr, lib) => {
-        arr.push({ type: 'file', value: `materials/${lib.name}/meta.json` })
+        arr.push({ type: 'file', value: `materials/${lib.name}/meta.json` });
         arr.push({
           type: 'file',
           value: `materials/${lib.name}/mergeMeta.json`,
-        })
-        return arr
-      }, [] as any[])
+        });
+        return arr;
+      }, [] as any[]);
 
-    projConfig.packOptions.ignore.push(...matertialMetaIgnores)
+    projConfig.packOptions.ignore.push(...matertialMetaIgnores);
 
     // # app.json, https://developers.weixin.qq.com/miniprogram/dev/reference/configuration/app.html
     if (appJson.tabBar) {
-      parseTabConfig(appJson.tabBar, ctx.projDir)
+      parseTabConfig(appJson.tabBar, ctx.projDir);
     }
-    merge(appConfig, appJson)
+    merge(appConfig, appJson);
 
     // # page.json
     // merge(pageConfigs, pagesConfigJson)
   }
 
   // keep main app config only, ignore subapp config
-  merge(appConfig, weapps[0].appConfig || {}, extractPages(weapps, pageConfigs))
+  merge(
+    appConfig,
+    weapps[0].appConfig || {},
+    extractPages(weapps, pageConfigs)
+  );
   // merge(pageConfigs, extractAllPagesConfig())
-  return { appConfig, projConfig, pageConfigs }
+  return { appConfig, projConfig, pageConfigs };
 }
 
 function extractPages(weapps: IWeAppData[], pageConfigs: any[]) {
-  const pages: string[] = []
-  const subPackages: any[] = []
-  let homePage = ''
+  const pages: string[] = [];
+  const subPackages: any[] = [];
+  let homePage = '';
   weapps.forEach((weapp, index) => {
-    const { rootPath } = weapp
+    const { rootPath } = weapp;
     const subPackage: {
-      root
-      pages: string[]
-    } = { root: rootPath, pages: [] }
+      root;
+      pages: string[];
+    } = { root: rootPath, pages: [] };
     if (rootPath) {
-      subPackages.push(subPackage)
+      subPackages.push(subPackage);
     }
     loopDealWithFn(weapp.pageInstanceList, (page) => {
-      const pageConfig = pageConfigs[index]
-      const pageFileName = get(pageConfig, `${page.id}.pageFileName`, 'index')
+      const pageConfig = pageConfigs[index];
+      const pageFileName = get(pageConfig, `${page.id}.pageFileName`, 'index');
       if (rootPath) {
-        subPackage.pages.push(`pages/${page.id}/${pageFileName}`)
+        subPackage.pages.push(`pages/${page.id}/${pageFileName}`);
       } else if (!page.isHome) {
-        pages.push(`pages/${page.id}/${pageFileName}`)
+        pages.push(`pages/${page.id}/${pageFileName}`);
       } else {
-        homePage = `pages/${page.id}/${pageFileName}`
+        homePage = `pages/${page.id}/${pageFileName}`;
       }
-    })
-  })
+    });
+  });
   if (homePage) {
-    pages.unshift(homePage)
+    pages.unshift(homePage);
   }
-  return { pages, subPackages }
+  return { pages, subPackages };
 }
 
 function getAppPagesConfig(pages: IWeAppPage[]) {
-  const pagesConfig = {}
+  const pagesConfig = {};
 
   pages.map((page) => {
-    const pageConfig = transformDynamicData(page.data)
-    delete pageConfig['title']
-    pagesConfig[page.id] = pageConfig
-  })
-  return pagesConfig
+    const pageConfig = transformDynamicData(page.data);
+    delete pageConfig['title'];
+    pagesConfig[page.id] = pageConfig;
+  });
+  return pagesConfig;
 }
 
 function transformDynamicData(originData) {
-  const temp = {}
+  const temp = {};
   for (const key in originData) {
-    const target = originData[key]
+    const target = originData[key];
     if (target && target.value) {
-      temp[key] = target.value
+      temp[key] = target.value;
     }
   }
-  return temp
+  return temp;
 }
 
 function parseTabConfig(tabBar, projDir: string) {
   tabBar.list.map((tab, index) => {
-    const { iconPath, selectedIconPath } = tab
-    tab.iconPath = 'assets/tab' + index + '/icon' + path.extname(iconPath)
+    const { iconPath, selectedIconPath } = tab;
+    tab.iconPath = 'assets/tab' + index + '/icon' + path.extname(iconPath);
     tab.selectedIconPath =
-      'assets/tab' + index + '/selectedIcon' + path.extname(selectedIconPath)
+      'assets/tab' + index + '/selectedIcon' + path.extname(selectedIconPath);
 
-    downloadFile(iconPath, projDir + '/' + tab.iconPath)
-    downloadFile(selectedIconPath, projDir + '/' + tab.selectedIconPath)
-  })
+    downloadFile(iconPath, projDir + '/' + tab.iconPath);
+    downloadFile(selectedIconPath, projDir + '/' + tab.selectedIconPath);
+  });
 }
