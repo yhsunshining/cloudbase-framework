beefd9b6fec33b3245c0106def8b03fa87a8ac1f
yang <yhunshining@gmail.com>
2021-08-13 11:55:41 +0800

    feat: material-action 兼容

--- packages/framework-plugin-low-code/template/src/app/material-actions.js
+++ packages/framework-plugin-low-code/template/src/app/material-actions.js
@@ -1,7 +1,7 @@
 // Imports all material actions
-<% materials.map(material => { if(material.entry){ %>
-import <%= _.camelCase(material.name)%> from 'libraries/<%=material.name%>@<%= material.version %>/<%=material.entry%>';
-const <%= _.camelCase(material.name)%>Actions = <%= _.camelCase(material.name)%>.actions
+<% materials.map((material,index) => { if(material.entry){ %>
+import <%= _.camelCase(material.name)%>_<%= index%> from 'libraries/<%=material.name%>@<%= material.version %>/<%=material.entry%>';
+const <%= _.camelCase(material.name)%>Actions = <%= _.camelCase(material.name)%>_<%= index%>.actions
 <% } else { (material.actions || []).map(act => {%>
 import <%= _.camelCase(material.name)%>_<%=act.name%> from 'libraries/<%=material.name%>@<%= material.version %>/actions/<%= act.name %>'
 <% }) }}) %>
