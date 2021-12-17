0751cedd721097139cb92fd23008c4d8af0fdcaa
yang <yhunshining@gmail.com>
2021-12-17 08:41:10 +0800

    0.6.1

--- packages/framework-plugin-low-code/template/src/app/material-actions.js
+++ packages/framework-plugin-low-code/template/src/app/material-actions.js
@@ -1,13 +1,15 @@
 // Imports all material actions
-<% materials.map((material,index) => { if(material.entry){ %>
-import <%= _.camelCase(material.name)%>_<%= index%> from 'libraries/<%=material.name%>@<%= material.version %>/<%=material.entry%>';
+<% materials.map((material,index) => { if(material.entries && material.entries.actions){ %>
+import * as <%= _.camelCase(material.name)%>Actions_<%= index%> from 'libraries/<%=material.name%>@<%= material.version %>/<%=material.entries.actions%>';
+<% } else if(material.entries && material.entries.entry) {%>
+import <%= _.camelCase(material.name)%>_<%= index%> from 'libraries/<%=material.name%>@<%= material.version %>/<%=material.entries.entry%>';
 const <%= _.camelCase(material.name)%>Actions_<%= index%> = <%= _.camelCase(material.name)%>_<%= index%>.actions
 <% } else { (material.actions || []).map(act => {%>
 import <%= _.camelCase(material.name)%>_<%=act.name%> from 'libraries/<%=material.name%>@<%= material.version %>/actions/<%= act.name %>'
 <% }) }}) %>
 export default {
   <% materials.map((material, index) => {
-  %>  ['<%= material.name%>']:<% if(material.entry){ %> <%=_.camelCase(material.name)%>Actions_<%= index%>,
+  %>  ['<%= material.name%>']:<% if(material.entries && (material.entries.entry || material.entries.actions)){ %> <%=_.camelCase(material.name)%>Actions_<%= index%>,
   <% }else{ %> {<% (material.actions || []).map(act => { %>
       ['<%= act.name%>']: <%= _.camelCase(material.name)%>_<%=act.name%>,<%})%>
     },
